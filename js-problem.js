de47e4b506d6e0b01d2e6a8283114658ae3d8e56
TanvirFahimBD <tanvirfahim6939@gmail.com>
2023-03-23 23:12:35 +0600

    comment in code

--- js-problem.js
+++ js-problem.js
@@ -1,13 +1,24 @@
+// a program that generates a multiplication table for a given number using a for loop.
+
+
+// multiplicationTable name function takes a single argument "num"
 function multiplicationTable(num) {
+    // check parameter if number 
     if (!isNaN(num)) {
+        // for loop iterates from 1 to 10
         for (let iteration = 1; iteration <= 10; iteration++) {
+            // multiplying the current iteration number by the input "num"
             let product = iteration * num;
+            // consoling the product in the format "num x iteration = product"
             console.log(num, 'x', iteration, " = ", product);
         }
-    } else {
+    }
+    // check parameter if not number
+    else {
         console.log('Please enter a number');
     }
 }
 
+// function call by passing single argument
 multiplicationTable(3)
 
